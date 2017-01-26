32baef3aa7d17799fefd93fd7106737d9c5d6fa0
LGM1213 <lgm1213@gmail.com>
2017-01-26 15:32:12 -0500

    corrected syntax errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 }, format: {with: VALID_EMAIL_REGEX}, uniqueness: { case_sensitive: false}
   has_secure_password
-  validates :password, presence: true, length { minumum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
