14e022b19ac600b0449b78496d0297ea8bfb8b66
LGM1213 <lgm1213@gmail.com>
2017-01-26 11:23:59 -0500

    updpated user model to work wiht bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,3 @@
-class User < ActiveRecord::Base
-  
+class User < ActiveRecord::Base  
   has_secure_password
-  # users.password_hash in the database is a :string
-  include BCrypt
-
-  def password
-    @password ||= Password.new(password_hash)
-  end
-
-  def password=(new_password)
-    @password = Password.create(new_password)
-    self.password_hash = @password
-  end
 end
