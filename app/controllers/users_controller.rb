347b3432fbf46721e5536cd65d6f08275be0fa81
LGM1213 <lgm1213@gmail.com>
2017-01-27 13:57:47 -0500

    added user show to controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,21 @@ class UsersController < ApplicationController
   def new
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
   def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
-      redirect_to '/'
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      flash[:success] = "Welcome to QuickScrim!"
+      redirect_to @user
     else
-      redirect_to '/signup'
+      render 'new'
     end
+  end
+
+  def edit
   end
 
 private
