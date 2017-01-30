13949f5942d85f6d2285d5992e524f6c966df8f1
LGM1213 <lgm1213@gmail.com>
2017-01-30 11:45:46 -0500

    added render file for error messages and updated sessions controller to include flash message when unable to log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,6 +14,7 @@ class SessionsController < ApplicationController
       redirect_to '/'
     else
     # If user's login doesn't work, send them back to the login form.
+      flash.now[:danger] = "Invalid email/password combination"
       redirect_to '/login'
     end
   end
