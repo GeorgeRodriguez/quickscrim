3fd7a9f4b7370b4530e92da4f621999c49e815d0
GeorgeRodriguez <georgesebastian2593@gmail.com>
2017-02-01 12:25:38 -0500

    remove migraton

--- db/migrate/20170125002319_create_direct_messages.rb
+++
@@ -1,8 +0,0 @@
-class CreateDirectMessages < ActiveRecord::Migration[5.0]
-  def change
-    create_table :direct_messages do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170126191046) do
+ActiveRecord::Schema.define(version: 20170127211831) do
 
   create_table "direct_messages", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text     "body"
+    t.integer  "friendships_id"
+    t.integer  "users_id"
+    t.boolean  "read",           default: false
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.index ["friendships_id"], name: "index_direct_messages_on_friendships_id"
+    t.index ["users_id"], name: "index_direct_messages_on_users_id"
   end
 
   create_table "friendships", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.integer  "sender_id"
+    t.integer  "recipient_id"
   end
 
   create_table "landings", force: :cascade do |t|
